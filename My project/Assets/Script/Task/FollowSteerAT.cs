3ef5ebfacd04c2749e9c0c6aeda95ed8acadbdc9
LungMilk <joeplace913@gmail.com>
2025-04-05 20:03:11 -0400

    modified steering behav

--- My project/Assets/Script/Task/FollowSteerAT.cs
+++ My project/Assets/Script/Task/FollowSteerAT.cs
@@ -1,16 +1,27 @@
 using NodeCanvas.Framework;
+using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowSteerAT : ActionTask
 {
+    [SerializeField] private Seeker seekAI;
     public BBParameter<Transform> target;
     public float stoppingDistance;
     public BBParameter<Vector3> accel;
     public float steeringAccel;
+
+    protected override string OnInit()
+    {
+        seekAI = agent.GetComponent<Seeker>();
+        return null;
+    }
     protected override void OnExecute()
     {
+        SetPath();
+
         float distanceToTarget = Vector3.Distance(agent.transform.position, target.value.position);
         if (distanceToTarget < stoppingDistance)
         {
@@ -21,6 +32,10 @@ public class FollowSteerAT : ActionTask
         direction = new Vector3(direction.x, 0f, direction.z);
         accel.value += direction.normalized * steeringAccel * Time.deltaTime;
         EndAction(true);
+    }
+    public void SetPath()
+    {
+        seekAI.StartPath(agent.transform.position,target.value.position);
     }
 
 }
